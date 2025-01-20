e5bb28f58d77e5ce77a7f790c63cdbc3f86da1b0
Jack Norris <hamuelxd@gmail.com>
2025-01-20 15:25:19 +0800

    Initial commit

---
+++ karbon/build.gradle.kts
@@ -0,0 +1,49 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.vanniktech.mavenPublish)
+}
+
+group = "com.thebrownfoxx"
+version = "0.1.0-alpha01"
+
+kotlin {
+    jvm()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    linuxX64()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                //put your multiplatform dependencies here
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+    }
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = group.toString(),
+        artifactId = "karbon",
+        version = version.toString(),
+    )
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    pom {
+        name = "Karbon"
+        description = "A Kotlin DSL for composing Markdown"
+        inceptionYear = "2024"
+        url = "https://github.com/thebrownfoxx/karbon"
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "karbon-dsl"
+include(":karbon")
