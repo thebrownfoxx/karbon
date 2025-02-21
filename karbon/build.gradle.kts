be3897b2588fbef11ab97563f7a2bea37d16b45e
Jack Norris <hamuelxd@gmail.com>
2025-02-21 13:22:41 +0800

    Removed Maven publishing of Karbon (non-14)

--- karbon/build.gradle.kts
+++ karbon/build.gradle.kts
@@ -1,6 +1,5 @@
 @file:OptIn(ExperimentalWasmDsl::class)
 
-import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
@@ -43,41 +42,41 @@ kotlin {
     }
 }
 
-mavenPublishing {
-    coordinates(
-        groupId = group.toString(),
-        artifactId = "karbon",
-        version = version.toString(),
-    )
-
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-
-    signAllPublications()
-
-    pom {
-        name = "Karbon"
-        description = "A Kotlin DSL for composing Markdown"
-        inceptionYear = "2024"
-        url = "https://github.com/thebrownfoxx/karbon"
-
-        licenses {
-            license {
-                name = "The Apache License, Version 2.0"
-                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-            }
-        }
-        developers {
-            developer {
-                id = "thebrownfoxx"
-                name = "Hamuel Agulto"
-                url = "https://github.com/thebrownfoxx"
-            }
-        }
-        scm {
-            url = "https://github.com/thebrownfoxx/karbon"
-            connection = "scm:git:git://github.com/thebrownfoxx/karbon.git"
-            developerConnection = "scm:git:ssh://git@github.com/thebrownfoxx/karbon.git"
-        }
-    }
-}
+//mavenPublishing {
+//    coordinates(
+//        groupId = group.toString(),
+//        artifactId = "karbon",
+//        version = version.toString(),
+//    )
+//
+//    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+//
+//    signAllPublications()
+//
+//    pom {
+//        name = "Karbon"
+//        description = "A Kotlin DSL for composing Markdown"
+//        inceptionYear = "2024"
+//        url = "https://github.com/thebrownfoxx/karbon"
+//
+//        licenses {
+//            license {
+//                name = "The Apache License, Version 2.0"
+//                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+//                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+//            }
+//        }
+//        developers {
+//            developer {
+//                id = "thebrownfoxx"
+//                name = "Hamuel Agulto"
+//                url = "https://github.com/thebrownfoxx"
+//            }
+//        }
+//        scm {
+//            url = "https://github.com/thebrownfoxx/karbon"
+//            connection = "scm:git:git://github.com/thebrownfoxx/karbon.git"
+//            developerConnection = "scm:git:ssh://git@github.com/thebrownfoxx/karbon.git"
+//        }
+//    }
+//}
